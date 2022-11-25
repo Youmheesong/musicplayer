32937c9d2f761799ec16fcc6dfe83e2bc635f2f9
Youmheesong <119025543+Youmheesong@users.noreply.github.com>
2022-11-25 13:57:35 +0900

    버튼추가

--- sketch.js
+++ sketch.js
@@ -1,15 +1,39 @@
-let mySound;
+// let mySound;
+// function setup(){
+//     soundFormats('mp3','ogg');
+//     mySound = loadSound('Pink Venom');
+//     mySound.play();
+// }
+
+// function loadmusic(){
+//     mySound.play();
+//     }
+
+// function draw(){
+//     fill(255,0,0);
+//     ellipse(50,50,100,100);
+// }
+
+var song;
+var button;
+
 function setup(){
-    soundFormats('mp3','ogg');
-    mySound = loadSound('Pink Venom');
-    mySound.play();
+    createCanvas(200,200);
+    song = loadSound('Pink Venom.mp3',loaded);
+    button.mousePressed(togglePlaying);
+    background(51);
 }
 
-function loadmusic(){
-    mySound.play();
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        button.html('pause');
+    }else{
+        song.stop();
+        button.html('play');
     }
 
-function draw(){
-    fill(255,0,0);
-    ellipse(50,50,100,100);
+function loaded(){
+    console.log('loaded');
 }
