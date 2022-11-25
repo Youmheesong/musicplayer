756c1f6d9f05668440b55643e61aa53cd9698374
Youmheesong <119025543+Youmheesong@users.noreply.github.com>
2022-11-25 13:34:15 +0900

    수정

--- start/sketch.js
+++ start/sketch.js
@@ -1,6 +1,10 @@
 let mySound;
 function setup(){
     soundFormats('mp3','ogg');
-    mySound = loadSound('Pink Venom.mp3');
+    mySound = loadSound('Pink Venom', loadMusic);
     mySound.play();
 }
+
+function loadmusic(){
+    mySound.play();
+    }
